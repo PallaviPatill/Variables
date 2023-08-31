ed5cd166f1795f50ada5b991df61b153f9053e41
unknown <pallavinirna@gmail.com>
2023-08-31 15:55:21 +0530

    initial commit

---
+++ laptop.java
@@ -0,0 +1,99 @@
+public class laptop{
+
+public static void main(String[] args){
+
+	String laptopname="acer";
+	int laptopprice=45000;
+	String laptopcolor="black";
+	String laptopRAM="8GB";
+	
+	System.out.println(laptopname);
+	System.out.println(laptopprice);
+	System.out.println(laptopcolor);
+	System.out.println(laptopRAM);
+
+	String jetname="airways";
+	int jetwings=4;
+	double jetprice=15.3;
+	boolean isjetavailable=true;
+
+	System.out.println(jetname);
+	System.out.println(jetwings);
+	System.out.println(jetprice);
+	System.out.println(isjetavailable);
+
+	String cameraname="sony";
+	int cameraprice=70000;
+	String camerastorage="256GB";
+	boolean iscamerahassensor=true;
+
+	System.out.println(cameraname);
+	System.out.println(cameraprice);
+	System.out.println(camerastorage);
+	System.out.println(iscamerahassensor);
+
+	String telephonename="apple";
+	int telephoneprice=5000;
+	String telephonecolor="white";
+	boolean istelephoneworking=false;
+
+	System.out.println(telephonename);
+	System.out.println(telephoneprice);
+	System.out.println(telephonecolor);
+	System.out.println(istelephoneworking);
+
+	String cosmeticname="lipstick";
+	String cosmeticcolor="nude";
+	int cosmeticprice=560;
+	String cosmeticbrand="loreal";
+
+	System.out.println(cosmeticname);
+	System.out.println(cosmeticcolor);
+	System.out.println(cosmeticprice);
+	System.out.println(cosmeticbrand);
+
+	String pillname="dolo";
+	int pillprice=50;
+	String pillcolor="pink";
+
+	System.out.println(pillname);
+	System.out.println(pillprice);
+	System.out.println(pillcolor);
+
+	String mousebrand="hp";
+	int mouseprice=550;
+	String mousecolor="purple";
+
+	System.out.println(mousebrand);
+	System.out.println(mouseprice);
+	System.out.println(mousecolor);
+
+	String fanname="crompton";
+	int fanprice=900;
+	String fancolor="blue";
+	int fanwings=3;
+
+	System.out.println(fanname);
+	System.out.println(fanprice);
+	System.out.println(fancolor);
+	System.out.println(fanwings);
+
+	String batbrand="gunn";
+	int batprice=1100;
+	String batcolor="cream";
+
+	System.out.println(batbrand);
+	System.out.println(batprice);
+	System.out.println(batcolor);
+
+	String foodname="vegrice";
+	int foodprice=90;
+	String foodcolor="white";
+
+	System.out.println(foodname);
+	System.out.println(foodprice);
+	System.out.println(foodcolor);
+
+}
+}
+	
